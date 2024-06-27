99a3a25fc2f903ebb594de3572cf25c10494f3cc
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-06-27 11:14:40 +0300

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from data import StellarBurgersUrl as Url
+
+
+def get_driver(name):
+    if name == "chrome":
+        from selenium.webdriver.chrome.service import Service as ChromeService
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+        return webdriver.Chrome(service=service)
+    elif name == "firefox":
+        from selenium.webdriver.firefox.service import Service as FirefoxService
+        service = FirefoxService(executable_path=GeckoDriverManager().install())
+        return webdriver.Firefox(service=service)
+    else:
+        raise TypeError("Driver is not found")
+
+
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    driver = get_driver(request.param)
+    driver.get(Url.URL_MAIN)
+    driver.maximize_window()
+    driver.delete_all_cookies()
+    yield driver
+    driver.quit()
---
+++ data.py
@@ -0,0 +1,17 @@
+class StellarBurgersUrl:
+
+    URL_MAIN = 'https://stellarburgers.nomoreparties.site/'
+    URL_LOGIN = f'{URL_MAIN}login'
+    URL_ACCOUNT = f'{URL_MAIN}account'
+    URL_ACCOUNT_PROFILE = f'{URL_MAIN}account/profile'
+    URL_FORGOT_PASSWORD = f'{URL_MAIN}forgot-password'
+    URL_RESET_PASSWORD = f'{URL_MAIN}reset-password'
+    URL_ORDER_HISTORY = f'{URL_MAIN}account/order-history'
+    URL_ORDER_FEED = f'{URL_MAIN}feed'
+
+
+
+class StellarBurgersLogin:
+
+    MY_LOGIN = 'pavel_pliasunov_8_888@yandex.ru'
+    MY_PASSWORD = '12345678'
